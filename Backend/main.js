12ea1b9795acae02b98de36adeb4d946b0dd5ced
noomuch <imnoomuch12@gmail.com>
2026-03-22 20:29:21 +0800

    fix main.js
    added .env

--- Backend/main.js
+++ Backend/main.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const mysql   = require('mysql2').createPool({
-    host:     'localhost',
-    port:     3306,
-    user:     'root',
-    password: 'REMOVED',
-    database: 'classroom_reservations'
+require('dotenv').config();
+const mysql = require('mysql2').createPool({
+    host:     process.env.DB_HOST,
+    port:     process.env.DB_PORT,
+    user:     process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 }).promise();
 const path = require('path');
 const app  = express();
