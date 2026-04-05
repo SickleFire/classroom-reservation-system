cea64a61123f142151467c3e8203412f079ac734
SickleFire <sicklefirex@gmail.com>
2026-04-05 19:30:56 +0800

    removed cors

--- Backend/main.js
+++ Backend/main.js
@@ -4,13 +4,8 @@ const bcrypt  = require('bcrypt');
 const mysql   = require('mysql2');
 const path    = require('path');
 require('dotenv').config();
-const cors = require('cors');
 
 const app = express();
-app.use(cors({
-    origin: 'https://classroom-reservation-system-production.up.railway.app',
-    credentials: true  // required for cookies/sessions
-}));
 app.set('trust proxy', 1);
 
 
